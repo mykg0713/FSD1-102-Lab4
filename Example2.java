4bec81213f9fb983c57d746e3f024f05dbe70da2
mykg0713 <51926251+mykg0713@users.noreply.github.com>
2019-08-27 19:46:35 -0700

    Add files via upload

--- Example2.java
+++ Example2.java
@@ -3,7 +3,9 @@ class Example2{
     public static void main(String args[]){
 
         Person p1 = new Person("Noah",27);
+        Person p2 = new Person("Troy",30);
         Employee e1 = new Employee(8572340,"Myk",37);
+        Employee e2 = new Employee(4000, "Kaite",35);
         Animal a1 = new Animal("Crocodile");
         Animal a2 = new Animal("Gorilla");
         Bonus b1 = new Bonus("a million dollars");
@@ -16,7 +18,11 @@ class Example2{
 
         System.out.println(e1.getName() + " receives " + b1.getName() + " plus " + e1.getSalary());
 
-        System.out.println(a2.getName() + " attacks " +p1.getName());
+        System.out.println(p2.getName() + " attacks " +p1.getName());
+
+        System.out.println(a2.getName() + " attacks " +p2.getName() +);
+
+        System.out.println(e2.getName() + " receives a salary of " + e2.getSalary() + " with a bonus of " + b1.getName());
 }
 
 }
